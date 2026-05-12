c63ba615eebfe3f17d355ece7e9ae8689f61cb48
limjihoon <lonelynight1026@gmail.com>
2026-05-13 00:36:03 +0900

    chore: update site title to Tony's Blog

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE_CONFIG = {
-  title: "Vanilla's Blog",
-  description: "A blog about software engineering, systems, and ideas.",
-  author: "vanillacake369",
-  email: "lonelynight1026@gmail.com",
-  github: "https://github.com/vanillacake369",
-  linkedin: "https://www.linkedin.com/in/vanillacake369/",
-  site: "https://vanillacake369.github.io",
+    title: "Tony's Blog",
+    description: "A blog about software engineering, systems, and ideas.",
+    author: "vanillacake369",
+    email: "lonelynight1026@gmail.com",
+    github: "https://github.com/vanillacake369",
+    linkedin: "https://www.linkedin.com/in/vanillacake369/",
+    site: "https://vanillacake369.github.io",
 } as const;
